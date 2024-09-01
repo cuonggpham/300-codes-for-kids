a100746d4485f02e5c355e3d46cac69743da1504
Pham Quoc Cuong <146796140+cuonggpham@users.noreply.github.com>
2024-09-02 00:57:59 +0700

    Time: 15 ms (67.40%) | Memory: 22 MB (25.99%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int p1 = 0;
+        int p2 = 0;
+        for(int p2=0; p2<nums.size(); p2++){
+            if(nums[p2] != 0){
+                swap(nums[p1], nums[p2]);
+                p1++;
+            }
+        }
+    }
+};
