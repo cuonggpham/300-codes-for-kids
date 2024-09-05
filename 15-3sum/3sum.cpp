9862673ca801023c36659b8b7deb89b273b4f333
Pham Quoc Cuong <146796140+cuonggpham@users.noreply.github.com>
2024-09-05 20:28:03 +0700

    Time: 298 ms (23.20%) | Memory: 35.2 MB (24.88%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-       int n= nums.size();
-       set<vector<int>> st;
-       sort(nums.begin(), nums.end());
-       
+        int n = nums.size();
+        set<vector<int>> se;
+        sort(nums.begin(), nums.end());
 
-      for(int i=0; i<n; i++){
-        int j=i+1;
-        int k=nums.size()-1;
-        vector<int> temp;
-        
+        for(int i=0; i<=n; i++){
+            int j = i+1;
+            int k = nums.size()-1;
+            vector<int> tmp;
 
-        while(j<k){
-            int sum= nums[i]+nums[j]+nums[k];
-            
-            if(sum==0){
-                temp.push_back(nums[i]);
-                temp.push_back(nums[j]);
-                temp.push_back(nums[k]);
-                st.insert(temp);
-                temp.clear();
-                j++;
-                k--;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                
+                if(sum == 0){
+                    tmp.push_back(nums[i]);
+                    tmp.push_back(nums[j]);
+                    tmp.push_back(nums[k]);
+                    se.insert(tmp);
+                    tmp.clear();
+                    j++;
+                    k--;
+                }
+                else if(sum < 0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
             }
-            else if(sum<0){
-                j++;
-               }
-            else{
-                k--;
-            }
-           }
-          }
-      vector<vector<int>> ans(st.begin(), st.end());
-     
-      return ans;
+        }
+        vector<vector<int>> ans(se.begin(), se.end());
+        return ans;
     }
 };
